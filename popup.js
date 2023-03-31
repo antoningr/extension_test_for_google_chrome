dca381ed36577c5b115c762cfb41cdd2d51081eb
Antonin Grimal <antonin.grimal@edu.esiee.fr>
2023-03-31 19:00:51 +0200

    commit update projet

--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
-console.log("L'extension de test fonctionne !");
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('myButton');
+    button.addEventListener('click', function() {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var tab = tabs[0];
+        var title = tab.title;
+        alert(title);
+      });
+    });
+  });
+  
